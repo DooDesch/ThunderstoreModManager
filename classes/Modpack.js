dd37d4a849806b8c40bb919bc57dd7f7127daac9
DooDesch <doodesch@gmail.com>
2023-04-10 21:31:38 +0200

    Update Modpack class

--- classes/Modpack.js
+++ classes/Modpack.js
@@ -3,33 +3,34 @@ const fs = require('fs');
 const AdmZip = require('adm-zip');
 
 class Modpack {
-    constructor() { }
+    iconFileName = "icon.png";
+    readmeFileName = "README.md";
+    manifestFileName = "manifest.json";
+    modpackDirectory = process.env.MODPACK_FOLDER || "./modpack";
+
+    constructor() {
+        const check = [
+            this.modpackDirectory,
+            path.join(this.modpackDirectory, this.manifestFileName),
+            path.join(this.modpackDirectory, this.readmeFileName),
+            path.join(this.modpackDirectory, this.iconFileName),
+        ]
+
+        this.filesOrDirectoriesExistOrError(check);
+    }
 
     createModpack() {
         return new Promise(async (resolve, reject) => {
             try {
-                const iconFileName = "icon.png";
-                const readmeFileName = "README.md";
-                const manifestFileName = "manifest.json";
-                const modpackDirectory = process.env.MODPACK_FOLDER || "./modpack";
-
-                const check = [
-                    modpackDirectory,
-                    path.join(modpackDirectory, manifestFileName),
-                    path.join(modpackDirectory, readmeFileName),
-                    path.join(modpackDirectory, iconFileName),
-                ]
-                this.filesOrDirectoriesExistOrError(check);
-
                 // Get modpack name from manifest
-                const manifestFileContent = fs.readFileSync(path.join(modpackDirectory, manifestFileName), 'utf-8');
+                const manifestFileContent = fs.readFileSync(path.join(this.modpackDirectory, this.manifestFileName), 'utf-8');
                 const manifest = JSON.parse(manifestFileContent);
                 const modpackName = manifest.name;
                 const modpackVersion = manifest.version_number;
 
                 // Create zip file using adm-zip
                 const zip = new AdmZip();
-                const folderToZip = modpackDirectory;
+                const folderToZip = this.modpackDirectory;
                 const zipFileName = `${modpackName}-${modpackVersion}.zip`;
                 const dist = process.env.MODPACK_DIST_FOLDER || "./dist";
                 const zipFilePath = path.join(dist, zipFileName);
