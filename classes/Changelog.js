e6081b82a4bbac0c960e30dcc8ea6e8cd05722c5
DooDesch <doodesch@gmail.com>
2023-04-16 20:59:10 +0200

    Fix updates in changelog

--- classes/Changelog.js
+++ classes/Changelog.js
@@ -6,6 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 
 export default class Changelog {
     initialized = false;
+    additions = [];
+    updates = [];
+    removals = [];
 
     constructor() {
         this.changelog = "## 1.0.0\n\n- Initial release";
@@ -29,7 +32,11 @@ export default class Changelog {
     async updateChangelog(version, { additions, updates, removals }) {
         await this.init();
 
-        const changes = additions.length + updates.length + removals.length;
+        this.additions = additions;
+        this.updates = updates;
+        this.removals = removals;
+
+        const changes = this.additions.length + this.updates.length + this.removals.length;
 
         if (version === "1.0.0") return new Promise((resolve) => { resolve(); });
         if (await this.versionExists(version)) return new Promise((resolve) => { resolve(); });
@@ -37,25 +44,27 @@ export default class Changelog {
 
         console.log(`[${path.basename(__filename)}] :: Updating changelog for version ${version}, adding ${changes} changes...`);
 
+        this.resolveAdditionsAndRemovals();
+
         let changelog = `## ${version}\n\n`;
 
-        if (additions.length > 0) {
+        if (this.additions.length > 0) {
             changelog += "- Added:\n";
-            for (const addition of additions) {
+            for (const addition of this.additions) {
                 changelog += `  - ${addition}\n`;
             }
         }
 
-        if (updates.length > 0) {
+        if (this.updates.length > 0) {
             changelog += "- Updated:\n";
-            for (const update of updates) {
+            for (const update of this.updates) {
                 changelog += `  - ${update}\n`;
             }
         }
 
-        if (removals.length > 0) {
+        if (this.removals.length > 0) {
             changelog += "- Removed:\n";
-            for (const removal of removals) {
+            for (const removal of this.removals) {
                 changelog += `  - ${removal}\n`;
             }
         }
@@ -78,5 +87,45 @@ export default class Changelog {
         await this.init();
 
         return this.changelog.includes(`## ${version}`);
+    }
+
+    resolveAdditionsAndRemovals() {
+        // Seperate author, name and version, which are seperated by a minus sign
+        const additions = this.additions.map((addition) => {
+            const [author, name, version] = addition.split('-');
+            return { author, name, version };
+        });
+
+        // Seperate author, name and version, which are seperated by a minus sign
+        const removals = this.removals.map((removal) => {
+            const [author, name, version] = removal.split('-');
+            return { author, name, version };
+        });
+
+        // Find all additions that are also removals
+        const additionsAndRemovals = additions.filter((addition) => {
+            return removals.some((removal) => {
+                return removal.name === addition.name;
+            });
+        });
+
+        // Remove all additions that are also removals
+        this.additions = this.additions.filter((addition) => {
+            return !additionsAndRemovals.some((additionAndRemoval) => {
+                return additionAndRemoval.name === addition.split('-')[1];
+            });
+        });
+
+        // Remove all removals that are also additions
+        this.removals = this.removals.filter((removal) => {
+            return !additionsAndRemovals.some((additionAndRemoval) => {
+                return additionAndRemoval.name === removal.split('-')[1];
+            });
+        });
+
+        // Add all additions that are also removals to the updates array
+        this.updates.push(...additionsAndRemovals.map((additionAndRemoval) => {
+            return `${additionAndRemoval.author}-${additionAndRemoval.name} to version ${additionAndRemoval.version}`;
+        }));
     }
 }
