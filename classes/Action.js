2b97a5225edab43ee40ff7fa526a78d1d7c02f5f
DooDesch <doodesch@gmail.com>
2023-04-16 23:28:45 +0200

    Refactor start command to be endless

--- classes/Action.js
+++ classes/Action.js
@@ -1,6 +1,7 @@
 import PackageUpdater from '../thunderstore/PackageUpdater.js';
 import ThunderstorePackageHandler from './ThunderstorePackageHandler.js';
 import Modpack from './Modpack.js';
+import inquirer from 'inquirer';
 
 class Action {
     constructor() {
@@ -98,6 +99,109 @@ class Action {
             if (updateManifest) await this.createManifest();
             await modpack.createModpack();
 
+            resolve();
+        });
+    }
+
+    start() {
+        return new Promise(async (resolve) => {
+            const questions = [
+                {
+                    type: 'list',
+                    name: 'action',
+                    message: 'What do you want to do?',
+                    choices: [
+                        {
+                            name: 'Install a package',
+                            value: 'installPackageByName',
+                        },
+                        {
+                            name: 'Update all packages',
+                            value: 'updatePackages',
+                        },
+                        {
+                            name: 'Remove a package',
+                            value: 'removePackageByName',
+                        },
+                        {
+                            name: 'Create a manifest file',
+                            value: 'createManifest',
+                        },
+                        {
+                            name: 'Update your modpack',
+                            value: 'createManifest',
+                        },
+                        {
+                            name: 'Zip your modpack',
+                            value: 'createModpack',
+                        },
+                        {
+                            name: 'Exit',
+                            value: 'exit',
+                        },
+                    ],
+                },
+                {
+                    type: 'input',
+                    name: 'packageName',
+                    message: 'Enter the name of the package to install:',
+                    when: (answers) => ['installPackageByName'].includes(answers.action), // only ask this question if the "install" option is selected
+                },
+                {
+                    type: 'input',
+                    name: 'packageName',
+                    message: 'Enter the name of the package to update:',
+                    when: (answers) => ['updatePackageByName'].includes(answers.action), // only ask this question if the "install" option is selected
+                },
+                {
+                    type: 'input',
+                    name: 'packageName',
+                    message: 'Enter the name of the package to remove:',
+                    when: (answers) => answers.action === 'remove', // only ask this question if the "remove" option is selected
+                },
+                {
+                    type: 'confirm',
+                    name: 'download',
+                    message: 'Do you want to download the package/s?',
+                    default: true,
+                    when: (answers) => ['installPackageByName', 'updatePackageByName', 'updatePackages'].includes(answers.action), // only ask this question if the "install" option is selected
+                },
+                {
+                    type: 'confirm',
+                    name: 'updateManifest',
+                    message: 'Do you want to update the manifest file?',
+                    default: true,
+                    when: (answers) => answers.action === 'createModpack', // only ask this question if the "update" option is selected
+                }
+            ];
+
+            const { action: actionName, packageName, download, updateManifest } = await inquirer.prompt(questions);
+
+            if (actionName === 'exit') {
+                process.exit(0);
+            }
+
+            switch (actionName) {
+                case 'installPackageByName':
+                    await this.installPackageByName(packageName, download);
+                    break;
+                case 'updatePackages':
+                case 'updatePackageByName':
+                    await this.updatePackageByName(packageName, download);
+                    break;
+                case 'removePackageByName':
+                    await this.remove(packageName);
+                    break;
+                case 'createManifest':
+                    await this.createManifest();
+                    break;
+                case 'createModpack':
+                    await this.createModpack(updateManifest);
+                    break;
+            }
+
+            // Ask again
+            await this.start();
             resolve();
         });
     }
